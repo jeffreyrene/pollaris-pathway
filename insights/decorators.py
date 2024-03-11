b37701691b4358b3513c284ce1e802af0c375649
jeffreyrene <jeffreyrene575@gmail.com>
2024-03-12 00:00:00 +0500

    feat: update urls

---
+++ insights/decorators.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponseForbidden
+
+def role_required(required_role):
+    def decorator(view_func):
+        def _wrapped_view(request, *args, **kwargs):
+            if hasattr(request.user, 'userprofile') and request.user.userprofile.role == required_role:
+                return view_func(request, *args, **kwargs)
+            return HttpResponseForbidden("⛔ You are not authorized to access this page.")
+        return _wrapped_view
+    return decorator
